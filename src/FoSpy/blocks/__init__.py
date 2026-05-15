5589d53a171f6df88bce579bc3268e483a60f606
errthumt <errthumt@iastate.edu>
2026-05-15 13:27:52 -0500

    add serialization methods

--- src/FoSpy/blocks/__init__.py
+++ src/FoSpy/blocks/__init__.py
@@ -1,3 +1,12 @@
+
+
+class SubContainer:
+    def __init__(self):
+        pass
+    def __iter__(self):
+        return iter(self.__dict__)
+
+
 class SingleBlock:
     """
     A single key–value block parsed from a FOS file.
@@ -19,12 +28,21 @@ class SingleBlock:
 
     def __init__(self, blockDict):
         from ..parsing.validation import required_keys, optional_keys
+        from ..parsing.syntax import meta_keys as mk
+        blockDict = blockDict.copy()
+        
+        self._meta = SubContainer()
+
+        for attr, key in mk.items():
+            setattr(self._meta, attr, blockDict.pop(key,None))
+
         validators = required_keys.get(self.__class__,{}) | optional_keys.get(self.__class__,{})
 
-        self.sourceDict = {}
-        self.extras = {}
+        self._key_order = []
+        self.ext = SubContainer()
 
         for key, val in blockDict.items():
+            self._key_order.append(key)
             if key in validators:
                 validator = validators[key]
                 if isinstance(validator, type) and issubclass(validator, SingleBlock):
@@ -32,12 +50,49 @@ class SingleBlock:
                         if len(val) > 1:
                             raise ValueError(f"Block '{key}' must be a single block. It can only be constructed from a list of length 1.")
                         val = val[0]
-                self.sourceDict[key] = validator(val)
                 setattr(self, key, validator(val))
 
             else:
-                self.sourceDict[key] = val
-                self.extras[key] = val
+                setattr(self.ext, key, val)
+
+    def __getattr__(self, name):
+        try:
+            return getattr(self.ext, name)
+        except AttributeError:
+            raise AttributeError(
+                f"{type(self).__name__} '{self.name if self.name else '<name unknown>'}'"
+                f"has no attribute '{name!r}'."
+            )
+        
+    def serialize(self):
+        all_attrs = {}
+        out = {}
+
+        def try_serial(obj):
+            serialize = getattr(obj, "serialize", None)
+            if callable(serialize):
+                return obj.serialize()
+            return obj
+
+
+        for attr,val in self.__dict__.items():
+            if attr == "ext" and val is not None:
+                for ext_attr, ext_val in val.__dict__.items():
+                    all_attrs[ext_attr] = ext_val
+            elif not attr.startswith("_"):
+                all_attrs[attr] = val
+
+        
+        for key in self._key_order:
+            if key in all_attrs:
+                val = all_attrs.pop(key)
+                out[key] = try_serial(val)
+        
+        for key, val in all_attrs.items():
+            out[key] = try_serial(val)
+
+
+
 
 
 class ListBlock:
@@ -57,20 +112,25 @@ class ListBlock:
     """
     def __init__(self, blockList, cls):
 
-        self.items = []
+        self.objs = []
         for blockDict in blockList:
-            self.items.append(cls.from_blockList(blockDict))
+            self.objs.append(cls.from_blockList(blockDict))
 
     def __getitem__(self, idx):
-        return self.items[idx]
+        return self.objs[idx]
     
     def __len__(self):
-        return len(self.items)
+        return len(self.objs)
     
     def __iter__(self):
-        return iter(self.items)
+        return iter(self.objs)
+        
+    def serialize(self):
+        return [obj.serialize()[0] for obj in self]
     
+
     
+from .synthesis import *   
 from .data import *
 from .materials import *
 from .metadata import *
