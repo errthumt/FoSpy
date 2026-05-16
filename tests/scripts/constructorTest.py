db9d65371400070fe23afd18ba026746b82f6ace
errthumt <errthumt@iastate.edu>
2026-05-15 19:56:56 -0500

    add weight percent comments to materials

--- tests/scripts/constructorTest.py
+++ tests/scripts/constructorTest.py
@@ -16,11 +16,12 @@ def constructTest():
     #pp(new_dict)
     # pp(mySyn.treatments[-1].program[0].__dict__)
 
-    passed = new_dict == file_dict
+    diffs = deep_diff(file_dict,new_dict)
+    passed = diffs == []
     print(f"Checking file dict with serialized synthesis: {'passed' if passed else 'failed'}")
 
     if not passed:
-        pp(deep_diff(file_dict, new_dict))
+        pp(diffs)
 
     mySyn.reaction.nominal_mass = "300.0"
 
