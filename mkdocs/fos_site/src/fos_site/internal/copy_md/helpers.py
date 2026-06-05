c0f50b3ac68ebd86ae8ed7b582292399a5fe0640
errthumt <errthumt@iastate.edu>
2026-06-05 15:08:26 -0500

    fix os / in markdown generation

--- mkdocs/fos_site/src/fos_site/internal/copy_md/helpers.py
+++ mkdocs/fos_site/src/fos_site/internal/copy_md/helpers.py
@@ -7,7 +7,8 @@ def copy_md_code(src_path:Path, dest_path:Path, site_name:str):
     dest_text = (
         f"# {site_name}: Raw Code\n\n"
         f"There is a copy button in the top right of the code block.\n\n"
-        f"[Click here to go to the rendered version]({os.path.relpath(src_path, dest_path.parent)})\n\n"
+        f"[Click here to go to the rendered version]"
+        f"({os.path.relpath(src_path, dest_path.parent).replace("\\", "/")})\n\n"
         f"````markdown\n"
         f"{src_text}\n"
         f"````\n"
