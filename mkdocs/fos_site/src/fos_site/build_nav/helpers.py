6f47cb19011c0c73c6fcf026134b95bef7197e4c
errthumt <errthumt@iastate.edu>
2026-06-06 12:18:01 -0500

    docs: corrupted nav generation

--- mkdocs/fos_site/src/fos_site/build_nav/helpers.py
+++ mkdocs/fos_site/src/fos_site/build_nav/helpers.py
@@ -12,11 +12,12 @@ def build_full_nav(tree: dict) -> list:
     # Subfolders
     subfolders = [(k, v) for k, v in tree.items() if k != "."]
 
+    '''
     # Case 1: folder contains exactly one file and no subfolders → collapse
     if len(file_items) == 1 and not subfolders:
         page_name, path = file_items[0]
         nav_list.append({page_name: path})
-        return nav_list
+        return nav_list'''
 
     # Case 2: normal folder → list files first
     for page_name, path in file_items:
@@ -27,9 +28,9 @@ def build_full_nav(tree: dict) -> list:
         subnav = build_full_nav(subtree)
 
         # Collapse subfolder if it contains exactly one {name: path}
-        if len(subnav) == 1 and isinstance(subnav[0], dict):
-            nav_list.append({key: subnav[0]})
-        else:
-            nav_list.append({key: subnav})
+        # if len(subnav) == 1 and isinstance(subnav[0], dict):
+        #     nav_list.append({key: subnav[0]})
+        # else:
+        nav_list.append({key: subnav})
 
     return nav_list
