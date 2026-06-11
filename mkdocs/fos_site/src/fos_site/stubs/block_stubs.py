1ddc8b1eac40bafdd75b7591d1bb45ce69b63cbc
errthumt <errthumt@iastate.edu>
2026-06-11 00:18:13 -0500

    docs: some docstring cleanup

--- mkdocs/fos_site/src/fos_site/stubs/block_stubs.py
+++ mkdocs/fos_site/src/fos_site/stubs/block_stubs.py
@@ -73,24 +73,6 @@ def make_stubs():
                 all functions, classes, and variables, see the [full
                 documentation](../full/index.md).
 
-                ## Methods attached to any object
-                When [assigning an object as an
-                attribute][FoSpy.blocks.blocks.SingleBlock.__setattr__],
-                `SingleBlock`s attach special methods that are used to edit
-                their own comment metadata.
-
-                ---
-
-                ### `add_comments(self_attr, *comments)`
-                ::: FoSpy.blocks.blocks._add_comments_to_parent
-
-                ---
-
-                ### `clear_comments(self_attr)`
-                ::: FoSpy.blocks.blocks._clear_comments_from_parent
-
-                ---
-
                 ## Block Types in this Module
                 
             """
