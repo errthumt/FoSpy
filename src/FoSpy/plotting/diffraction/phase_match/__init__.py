b8bd1cd5752cc8f279ee3e0cce7eb2cfe808d31a
errthumt <errthumt@iastate.edu>
2026-06-11 21:17:07 -0500

    feat: continue with matching build

--- src/FoSpy/plotting/diffraction/phase_match/__init__.py
+++ src/FoSpy/plotting/diffraction/phase_match/__init__.py
@@ -1,19 +1,33 @@
 from pandas import DataFrame
 from ..engines import ENGINES
 from ....config import values as cfg
-class PhaseMatcher(DataFrame):
-    def __init__(self, exp_2theta, exp_int, *cif_attachments, engine=None,**kwargs):
+class PhaseMatcher:
+    def __init__(self, exp_2theta, exp_int, cif_dict, engine=None, x_name="two_theta", **kwargs):
         from .match import merge_frames
-        super().__init__({
+        self.x_name = x_name
+        df = DataFrame({
             "two_theta":exp_2theta, "intensity":exp_int
         })
 
         if engine is None:
             engine = cfg.diffraction.engine.default
 
-        default_engine_parameters = cfg.
+        engine_parameters = cfg.diffraction.engines.engine_defaults[engine].to_dict()
+        engine_parameters.update(kwargs)
+        self.engine_cls = ENGINES[engine]
 
-        self.engines = []
+        self.engines = {k:self.engine_cls(cif._get_filepath(), **engine_parameters) for k, cif in cif_dict.items()}
+
+        frames = {"exp":df,
+                  **{name: engine.get_pattern() for name, engine in self.engines.items()}}
+        
+        self.frame = merge_frames(x_name=x_name, normalize=False, **frames)
+
+    def match_peaks(self, **kwargs):
+        pass
+
+
+
 
 
 
