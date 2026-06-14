ec6295d0f9fefc88486851eb56a2ec7ea93955e2
errthumt <errthumt@iastate.edu>
2026-06-14 14:29:15 -0500

    feat: rudimentary interactive find_peaks window

--- src/FoSpy/plotting/diffraction/phase_match/_utils.py
+++ src/FoSpy/plotting/diffraction/phase_match/_utils.py
@@ -44,5 +44,34 @@ def plot_stick_at_x(x, x_array, y_array, ax=None, **vline_kwargs):
     
     y = y_array[nearest]
 
-    ax.vlines(x, 0, y, **vline_kwargs)
-    return ax
+    line = ax.vlines(x, 0, y, **vline_kwargs)
+    return line
+
+
+def get_find_sliders(find_cfg, intensity_col):
+    return {
+    "height": {
+        "type": "range", "label": "Peak Height", "min": 0, "max": 1, "default": find_cfg["height"]
+    },
+    "threshold": {
+        "type": "range", "label": "Threshold", "min": 0, "max": 1, "default": find_cfg["threshold"]
+    },
+    "distance": {
+        "type": "scalar", "label": "Min Distance (samples)", "min": 1, "max": 200, "default": find_cfg["distance"]
+    },
+    "prominence": {
+        "type": "range", "label": "Prominence", "min": 0, "max": 1, "default": find_cfg["prominence"]
+    },
+    "width": {
+        "type": "range", "label": "Width (samples)", "min": 0, "max": 200, "default": find_cfg["width"]
+    },
+    "wlen": {
+        "type": "scalar", "label": "Window Length", "min": 2, "max": len(intensity_col), "default": find_cfg["wlen"]
+    },
+    "rel_height": {
+        "type": "scalar", "label": "Relative Height", "min": 0, "max": 1, "default": find_cfg["rel_height"]
+    },
+    "plateau_size": {
+        "type": "range", "label": "Plateau Size", "min": 0, "max": 200, "default": find_cfg["plateau_size"]
+    },
+}
