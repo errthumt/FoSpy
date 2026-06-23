de6b28cf286d50032e725327651f5d9b6b6b95d1
errthumt <errthumt@iastate.edu>
2026-06-23 18:34:02 -0500

    test: configure test to check all example files for matching

--- tests/FoSpy/code_example/conftest.py
+++ tests/FoSpy/code_example/conftest.py
@@ -1,7 +1,6 @@
 import pytest
 
-@pytest.fixture(scope="function")
-def example_start(tmp_path_factory):
+def example_dir(temp_dir):
     from pathlib import Path
     import os
 
@@ -12,8 +11,6 @@ def example_start(tmp_path_factory):
         ("start_templates.fos", "templates"),
         ("PY618_Ba8-Cu12-Zn12-As29,8.cif", "templates"),
     )
-
-    temp_dir = tmp_path_factory.mktemp("example_dir")
 
     for from_file, to_dir in copy_files:
         from_path = current / from_file
@@ -26,12 +23,47 @@ def example_start(tmp_path_factory):
 
     return temp_dir
 
+
+
+@pytest.fixture(scope="function")
+def example_start(tmp_path_factory):
+    temp_dir = tmp_path_factory.mktemp("example_dir")
+
+    return example_dir(temp_dir)
+
+
 @pytest.fixture(scope="module")
-def example_finished(example_start):
+def example_frozen(tmp_path_factory):
+    from pathlib import Path
+    import os
+    import shutil
+
+    freeze = Path(os.path.abspath(__file__)).parent / "freeze"
+    temp_dir = tmp_path_factory.mktemp("example_frozen")
+
+    for folder in ("synthesis", "templates"):
+        for f in freeze.rglob(f"{folder}/*"):
+            rel_path = f.relative_to(freeze)
+            to_path = temp_dir / rel_path
+            to_path.parent.mkdir(parents=True, exist_ok=True)
+            shutil.copy2(f, to_path)
+
+    return temp_dir
+
+
+@pytest.fixture(scope="module")
+def example_finished(tmp_path_factory):
     from contextlib import chdir
-    with chdir(example_start):
+
+    temp_dir = tmp_path_factory.mktemp("example_finished")
+    temp_dir = example_dir(temp_dir)
+    with chdir(temp_dir):
         example()
-    return example_start
+    return temp_dir
+
+@pytest.fixture(scope="module")
+def example_relpaths(example_finished):
+    return [fp.relative_to(example_finished) for fp in example_finished.rglob("*.*")]
 
 # <fos_site> extract example code below.
 def example():
