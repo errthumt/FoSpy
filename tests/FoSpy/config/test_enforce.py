13b71509932d2d8eaadd13b6fe0384c8e46c1772
errthumt <errthumt@iastate.edu>
2026-06-23 15:44:05 -0500

    patch: skip config unit testing for now

--- tests/FoSpy/config/test_enforce.py
+++ tests/FoSpy/config/test_enforce.py
@@ -1,42 +1,42 @@
-import pytest
+# import pytest
 
-import FoSpy.config._enforce
-
-
-@pytest.mark.parametrize(
-    "expected",
-    [
-        # TODO: fill in test data for test_enforce_config
-        # pytest.param(id="")
-    ]
-)
-def test_enforce_config(expected):
-    # TODO: create assertions for test_enforce_config
-    # FoSpy.config._enforce.enforce_config()
-    pass
+# import FoSpy.config._enforce
 
 
-@pytest.mark.parametrize(
-    "module,checks,expected",
-    [
-        # TODO: fill in test data for test_add_enforcement
-        # pytest.param(, , id="")
-    ]
-)
-def test_add_enforcement(module, checks, expected):
-    # TODO: create assertions for test_add_enforcement
-    # FoSpy.config._enforce._add_enforcement(module, checks)
-    pass
+# @pytest.mark.parametrize(
+#     "expected",
+#     [
+#         # TODO: fill in test data for test_enforce_config
+#         # pytest.param(id="")
+#     ]
+# )
+# def test_enforce_config(expected):
+#     # TODO: create assertions for test_enforce_config
+#     # FoSpy.config._enforce.enforce_config()
+#     pass
 
 
-@pytest.mark.parametrize(
-    "enforced_dict,expected",
-    [
-        # TODO: fill in test data for test_generate_checks
-        # pytest.param(, id="")
-    ]
-)
-def test_generate_checks(enforced_dict, expected):
-    # TODO: create assertions for test_generate_checks
-    # FoSpy.config._enforce._generate_checks(enforced_dict)
-    pass
+# @pytest.mark.parametrize(
+#     "module,checks,expected",
+#     [
+#         # TODO: fill in test data for test_add_enforcement
+#         # pytest.param(, , id="")
+#     ]
+# )
+# def test_add_enforcement(module, checks, expected):
+#     # TODO: create assertions for test_add_enforcement
+#     # FoSpy.config._enforce._add_enforcement(module, checks)
+#     pass
+
+
+# @pytest.mark.parametrize(
+#     "enforced_dict,expected",
+#     [
+#         # TODO: fill in test data for test_generate_checks
+#         # pytest.param(, id="")
+#     ]
+# )
+# def test_generate_checks(enforced_dict, expected):
+#     # TODO: create assertions for test_generate_checks
+#     # FoSpy.config._enforce._generate_checks(enforced_dict)
+#     pass
