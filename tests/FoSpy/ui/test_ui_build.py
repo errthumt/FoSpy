11c4fec17f50b4d58ebf41dd764e34d6c04dbfde
errthumt <errthumt@iastate.edu>
2026-06-23 20:15:06 -0500

    test: rewrite ui tests to skip user input

--- tests/FoSpy/ui/test_ui_build.py
+++ tests/FoSpy/ui/test_ui_build.py
@@ -1,10 +1,24 @@
-def test_matplotlib_ui_build():
-    from FoSpy.ui.abstract import AssembleSlider
+import pytest
 
-    class EmptySlider:
-        pass
+class Empty:
+    def main_loop(self):
+        self._ok_clicked()
+
+@pytest.fixture
+def EmptySlider():
+    return Empty
+
+def test_matplotlib_ui_build(EmptySlider):
+    from FoSpy.ui.abstract import AssembleSlider
 
     ui = AssembleSlider(EmptySlider, ui='matplotlib')()
     
-    assert ui.main_loop() == None
+    assert ui.main_loop() is None
+
+def test_pyqtgraph_ui_build(EmptySlider):
+    from FoSpy.ui.abstract import AssembleSlider
+
+    ui = AssembleSlider(EmptySlider, ui='pyqtgraph')()
+    
+    assert ui.main_loop() is None
 
