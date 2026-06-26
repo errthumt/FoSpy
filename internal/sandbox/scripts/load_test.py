127a47c49ef461b62f873385700c7c97318b2caf
errthumt <errthumt@iastate.edu>
2026-06-26 14:57:45 -0500

    fix: bring example jsons back up to date

--- internal/sandbox/scripts/load_test.py
+++ internal/sandbox/scripts/load_test.py
@@ -1,3 +1,3 @@
 from FoSpy.blocks import Synthesis
 
-jill = Synthesis.fromFile(r"C:\Users\travi\FoSpy\user_files\testing\eln_data_maptest_2026-06-26_12-21-47\eln_data_maptest_original.json")
+jill = Synthesis.fromFile(r"C:\Users\travi\FoSpy\user_files\testing\eln_data_maptest_2026-06-26_13-28-49\eln_data_maptest_original.json")
