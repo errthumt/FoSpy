b99a291398adc5c2b7a0fbd11c23952456623e50
errthumt <errthumt@iastate.edu>
2026-07-04 11:44:43 -0400

    fix: freeze new changes to example outputs

--- tests/FoSpy/code_example/test_example.py
+++ tests/FoSpy/code_example/test_example.py
@@ -5,4 +5,8 @@ def test_example_match(example_frozen, example_finished, example_relpaths, subte
         with subtests.test(msg=f"Checking {rel_path}"):
             frozen_path = example_frozen / rel_path
             finished_path = example_finished / rel_path
-            assert frozen_path.read_bytes() == finished_path.read_bytes()
+
+            frozen_lines = frozen_path.read_text().splitlines()
+            finished_lines = finished_path.read_text().splitlines()
+
+            assert frozen_lines == finished_lines
